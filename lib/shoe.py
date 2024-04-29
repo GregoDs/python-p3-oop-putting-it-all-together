396020bf8bc7e18d9582eeb20e1c9b520bb5db9e
+ <+>
2024-04-29 15:44:30 +0300

    Completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+#Shoe in shoe.py prints "size must be an integer" if size is not an integer.
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if (isinstance(size,int)):
+            self._size  = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size,set_size)
+# Shoe in shoe.py says that the shoe has been repaired.
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    
+
